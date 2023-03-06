dc3e2ca78991c037817f280a5dbdbdd8638a5fc1
Dawid Osiński <dawidosinski59@gmail.com>
2023-03-06 19:17:08 +0100

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,58 @@
 {
-  const tasks = [
-    {
-      content: "nagrać lekcję",
-      done: false,
-    },
-    {
-      content: "zjeść pierogi",
-      done: true,
-    },
-  ];
+    const tasks = [
+        {
+            content: "nagrać lekcję",
+            done: false,
+        },
+        {
+            content: "zjeść pierogi",
+            done: true,
+        },
+    ];
 
-  const render = () => {
-    let htmlString = "";
+    const render = () => {
+        let htmlString = "";
 
-    for (const task of tasks) {
-      htmlString += `<li>${task.content}</li>`;
-    }
+        for (const task of tasks) {
+            htmlString += `
+      <li 
+        ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+        >
+        ${task.content}
+      </li>
+      `;
+        }
 
-    document.querySelector(".js-tasks").innerHTML = htmlString;
-  };
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
 
-  const init = () => {
-    render();
-  };
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
 
-  init();
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
 }
