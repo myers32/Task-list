97ec31605c4441cc0afde53abba60a207316daa9
Dawid Osiński <dawidosinski59@gmail.com>
2023-03-07 14:58:31 +0100

    new classes and css added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 {
-    const tasks = [
-        {
-            content: "nagrać lekcję",
-            done: false,
-        },
-        {
-            content: "zjeść pierogi",
-            done: true,
-        },
-    ];
+    const tasks = [];
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
